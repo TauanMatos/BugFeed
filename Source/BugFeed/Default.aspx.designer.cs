c216209a799d55e1efebf58db634d2369777e126
Victor Brito <victorsebrito@users.noreply.github.com>
2018-06-10 17:46:20 -0300

    Landing page

--- Source/BugFeed/Default.aspx.designer.cs
+++ Source/BugFeed/Default.aspx.designer.cs
@@ -11,5 +11,32 @@ namespace BugFeed {
     
     
     public partial class Default {
+        
+        /// <summary>
+        /// body control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl body;
+        
+        /// <summary>
+        /// loggedOutMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl loggedOutMenu;
+        
+        /// <summary>
+        /// loggedInMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl loggedInMenu;
     }
 }
